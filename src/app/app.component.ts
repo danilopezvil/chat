d9bcb33e2f4b2b1827fc1dbb2024b350e8567a17
Danilo Lopez <danilo.lopez@simplecero.com>
2019-05-24 17:39:12 -0500

    Guardando la version 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MessageService } from './services/message.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat';
+  message = '';
+  chats = [
+    'Lorem ipsum dolor sit',
+    'amet consectetur adipisicing elit',
+    'Obcaecati neque odit delectus',
+    'error ducimus sequi dolorem',
+    'nihil at provident architecto',
+    'magnam earum possimus',
+    'itaque adipisci veritatis',
+    'aut, autem tenetur hic.'];
+
+    constructor(message: MessageService) {
+
+    }
+
+    addChat() {
+      if (this.message.length === 0 ) {
+        return;
+      }
+      this.chats.push(this.message);
+      this.message = '';
+      window.setInterval(() => {
+        const elem = document.getElementById('scrolldiv');
+        elem.scrollTop = elem.scrollHeight;
+      }, 500);
+    }
 }
---
+++ src/app/services/message.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  socket;
+  constructor() {
+    this.socket = io.connect();
+  }
+}
